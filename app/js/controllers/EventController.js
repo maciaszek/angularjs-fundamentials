661dd7f5c0451e2926f2e4aa814b58e56d23b394
MaciejD <maciej.drazek@o2.pl>
2016-08-10 23:03:20 +0200

    handling events

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -18,23 +18,34 @@ eventsApp.controller('EventController',
                     creatorName: 'Bob Smith',
                     duration: '1 hr',
                     level: 'Advanced',
-                    abstract: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et facilisis metus, ac blandit metus. In consectetur ornare felis dictum lobortis. Aliquam nec est ac magna volutpat venenatis. Vestibulum vestibulum sed turpis eu suscipit.'
+                    abstract: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et facilisis metus, ac blandit metus. In consectetur ornare felis dictum lobortis. Aliquam nec est ac magna volutpat venenatis. Vestibulum vestibulum sed turpis eu suscipit.',
+                    upVoteCount: 0
                 },
                 {
                     name: 'Scopes for fun and profit',
                     creatorName: 'Jhon Doe',
                     duration: '30 mins',
                     level: 'Introductory',
-                    abstract: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et facilisis metus, ac blandit metus. In consectetur ornare felis dictum lobortis. Aliquam nec est ac magna volutpat venenatis. Vestibulum vestibulum sed turpis eu suscipit.'
+                    abstract: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et facilisis metus, ac blandit metus. In consectetur ornare felis dictum lobortis. Aliquam nec est ac magna volutpat venenatis. Vestibulum vestibulum sed turpis eu suscipit.',
+                    upVoteCount: 0
                 },
                 {
                     name: "Well behaved controlles",
                     creatorName: 'Jane Doe',
                     duration: '2 hours',
                     level: 'Intermediate',
-                    abstract: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et facilisis metus, ac blandit metus. In consectetur ornare felis dictum lobortis. Aliquam nec est ac magna volutpat venenatis. Vestibulum vestibulum sed turpis eu suscipit.'
+                    abstract: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et facilisis metus, ac blandit metus. In consectetur ornare felis dictum lobortis. Aliquam nec est ac magna volutpat venenatis. Vestibulum vestibulum sed turpis eu suscipit.',
+                    upVoteCount: 0
                 }
             ]
+        };
+
+        $scope.upVoteSession = function(session){
+            session.upVoteCount++;
+        };
+
+        $scope.downVoteSession = function(session){
+            session.upVoteCount--;
         };
     }
 );
